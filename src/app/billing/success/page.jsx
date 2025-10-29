2206d034a20f50096e23b9ec26066522d7d5cd7f
Igor <igor.oliveira.contact2210@gmail.com>
2025-10-29 19:19:14 -0300

    forçando página de sucesso a ser dinamica

--- src/app/billing/success/page.jsx
+++ src/app/billing/success/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 import SuccessClient from "./SuccessClient";
 
+// Garantir que a página não será prerenderizada
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return <SuccessClient />;
 }
