7f16f74179162c3d104abf07f17add7cfc76f2d9
Igor <igor.oliveira.contact2210@gmail.com>
2026-01-10 20:23:40 -0300

    Comentando section do blog da landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -150,11 +150,11 @@ export default function Home() {
         </div>
       </section>
 
-      <div id="blog" className="pt-8">
+      {/* <div id="blog" className="pt-8">
         <div className="bg-translucid py-12 px-4 sm:px-8">
           <BlogSection />
         </div>
-      </div>
+      </div> */}
 
       <footer className="bg-low-gray p-6 border-t border-gray-300 dark:border-gray-700">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
