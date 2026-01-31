b7f5e3ccc650ef983f19420d25464592c6a84b3c
Igor <igor.oliveira.contact2210@gmail.com>
2026-01-31 13:08:08 -0300

    melhoria no design dos graficos do salesDashboard - 2

--- src/app/dashboard/tabs/components/SalesCharts.jsx
+++ src/app/dashboard/tabs/components/SalesCharts.jsx
@@ -74,7 +74,7 @@ export default function SalesCharts({ sales = [] }) {
       {filteredPayments.length === 0 && <p className="color-gray text-center">Nenhuma venda no período selecionado.</p>}
 
       {filteredPayments.length > 0 && (
-        <div className="xxs:min-w-[300px] xxs:mx-auto lg:mx-0 bg-translucid border border-translucid rounded-lg w-full p-4">
+        <div className="xxs:min-w-[300px] xxs:mx-auto lg:mx-0 bg-translucid border border-translucid rounded-lg w-full p-4 py-6 flex flex-col items-center md:items-start">
           <h3 className="font-semibold color-gray lg:text-center mb-6">Métodos de Pagamento</h3>
           <div className="flex items-center">
             <div className="w-26 h-26 xs:w-32 xs:h-32 lg:w-36 lg:h-36">
@@ -98,7 +98,7 @@ export default function SalesCharts({ sales = [] }) {
       )}
 
       {filteredServices.length > 0 && (
-        <div className="xxs:min-w-[300px] xxs:mx-auto lg:mx-0 bg-translucid border border-translucid rounded-lg w-full p-4">
+        <div className="xxs:min-w-[300px] xxs:mx-auto lg:mx-0 bg-translucid border border-translucid rounded-lg w-full p-4 py-6 flex flex-col items-center md:items-start">
           <h3 className="font-semibold color-gray lg:text-center mb-6">Tipos de Serviço</h3>
           <div className="flex items-center">
             <div className="w-26 h-26 xs:w-32 xs:h-32 lg:w-36 lg:h-36">
